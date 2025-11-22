667348c5d80843df789579261e9a67677db2be4b
tuz <tuz@mail.local>
2025-11-22 13:53:54 -0500

    update sql

--- Laboratorio20/laboratorio14-23-whitney.sql
+++ Laboratorio20/laboratorio14-23-whitney.sql
@@ -1,32 +1,32 @@
-use master
-go
+USE master
+GO
 
 DROP DATABASE Productos;
-go
+GO
 
-create database Productos;
-go
+CREATE database Productos;
+GO
 
-use Productos;
-go
+USE Productos;
+GO
 
-create table Laptops (
+CREATE TABLE Laptops (
 	id int not null identity,
 	nombre varchar(50) not null,
 	precio decimal(6,2),
 	stock float,
 	constraint pk_laptops primary key(id)
 );
-go
+GO
 
 
-SELECT 
-[id], 
-[nombre], 
-[precio], 
+SELECT
+[id],
+[nombre],
+[precio],
 [stock]
-FROM 
-[Productos].[dbo].[Laptops] 
-WHERE 
+FROM
+[Productos].[dbo].[Laptops]
+WHERE
 [id] = 2
 GO
